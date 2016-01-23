42399724f31a2f6ac798f81f3d1eac226d3598db
Lucas Marques <lucasmarquesds@gmail.com>
2016-01-23 12:32:00 -0200

    finished method to show files in a given path

--- files.py
+++ files.py
@@ -1,5 +1,20 @@
 def list_files(suffix, path, show_fullpath=False):
-    if show_fullpath:
-        pass
+    """
+    Search for files with given suffix in a path and list them.
+
+    Args:
+        suffix: File extension to be analysed. It works with strings, regex and tuple of strings.
+                If empty string, method will show all type of files in specified path.
+        path: Path to list files
+        show_fullpath: If this flag is True, method will return a list of filenames with
+                       fullpath of each file plus its name.
+
+    :return: A list of all files in a path
+    """
+    if os.path.exists(path):
+        if not show_fullpath:
+            return [f for f in listdir(path) if isfile(join(path, f)) and f.endswith(suffix)]
+        else:
+            return [os.path.join(path, f) for f in listdir(path) if isfile(join(path, f)) and f.endswith(suffix)]
     else:
-        pass
+        return []
